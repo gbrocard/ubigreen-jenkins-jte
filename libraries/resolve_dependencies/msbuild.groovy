2f889590b7b3766f2e443ee4757fb5dc5327f6a3
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-09-25 16:44:45 +0200

    print

--- libraries/resolve_dependencies/msbuild.groovy
+++ libraries/resolve_dependencies/msbuild.groovy
@@ -1,12 +1,21 @@
-@BeforeStep({context.step.equals("build")})
+@BeforeStep({ context.step.equals("build") })
 void call(context) {
+    println("test1")
+
+
     String sources = config.nugetSources.join(' -sources=')
+    println("test2")
+
     String nugetPath = config.nugetPath
+    println("test3")
+    
     String projectPath = config.project
+    println("test4")
 
     node {
         stage('Resolve Dependencies: NuGet') {
-            println("env:  " + env.WORKSPACE)
+            println("test5")
+         
             bat "\"${nugetPath}\" restore \"${projectPath}\" ${sources} -OutputDirectory ${env.WORKSPACE}\\packages"
         }
     }
