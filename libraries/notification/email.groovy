b589e6cd47db33c98643a1ef92e992874b38cc4a
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-09-25 16:41:01 +0200

    fix

--- libraries/notification/email.groovy
+++ libraries/notification/email.groovy
@@ -1,6 +1,6 @@
-@AfterStep({println(context)})
+@AfterStep({context.status.equals("FAILURE")})
 void call(context) {
-    def emailSubject = "${env.JOB_NAME} - Build #${env.BUILD_NUMBER} - ${context.status}!"
+    def emailSubject = "${env.JOB_NAME} - Build #${env.BUILD_NUMBER} - ${currentBuild.currentResult}!"
     def emailBody = "Check console output at ${env.BUILD_URL} to view the results"
     emailext body: emailBody, recipientProviders: [requestor(), buildUser(), developers(), recipients()], subject: emailSubject, from: "DevOps <devops@ubigreen.com>"
 }
