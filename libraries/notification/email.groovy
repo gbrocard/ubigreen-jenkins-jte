4cec3b6d0ec41c15aa5d764aff42f7d0a0f4a607
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-09-25 16:28:44 +0200

    Revert "print"
    This reverts commit e5ec25895e57c81a7378371e82505bf9c47e3e7c.

--- libraries/notification/email.groovy
+++ libraries/notification/email.groovy
@@ -1,7 +1,6 @@
 @AfterStep({context.status.equals("FAILURE")})
 void call(context) {
-    println "Hello"
-    // def emailSubject = "${env.JOB_NAME} - Build #${env.BUILD_NUMBER} - ${context.status}!"
-    // def emailBody = "Check console output at ${env.BUILD_URL} to view the results"
-    // emailext body: emailBody, recipientProviders: [requestor(), buildUser(), developers(), recipients()], subject: emailSubject, from: "DevOps <devops@ubigreen.com>"
+    def emailSubject = "${env.JOB_NAME} - Build #${env.BUILD_NUMBER} - ${context.status}!"
+    def emailBody = "Check console output at ${env.BUILD_URL} to view the results"
+    emailext body: emailBody, recipientProviders: [requestor(), buildUser(), developers(), recipients()], subject: emailSubject, from: "DevOps <devops@ubigreen.com>"
 }
