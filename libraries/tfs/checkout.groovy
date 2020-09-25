95ad70b92d10804565ab6e4ff8dc3757cf530c48
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-09-25 15:44:22 +0200

    added node

--- libraries/tfs/checkout.groovy
+++ libraries/tfs/checkout.groovy
@@ -1,22 +1,24 @@
 void call() {
-    stage('Checkout: TFS') {
-        options {
-            timeout(time: 15, unit: 'MINUTES')
-            retry(1)
-        }
+    node {
+        stage('Checkout: TFS') {
+            options {
+                timeout(time: 15, unit: 'MINUTES')
+                retry(1)
+            }
 
-        environment {
-            PROJECT = '$/UbigreenProject/UbigreenPerformance'
-            TFS_URL = 'http://tls-ubi-tfs:8080/tfs/UbigreenCollection'
-        }
+            environment {
+                PROJECT = '$/UbigreenProject/UbigreenPerformance'
+                TFS_URL = 'http://tls-ubi-tfs:8080/tfs/UbigreenCollection'
+            }
 
-        checkout([
-            $class: 'TeamFoundationServerScm', 
-            projectPath: '$/UbigreenProject/UbigreenPerformance',
-            credentialsConfigurer: [$class: 'AutomaticCredentialsConfigurer'],
-            serverUrl: 'http://tls-ubi-tfs:8080/tfs/UbigreenCollection', 
-            useOverwrite: true,
-            workspaceName: "Hudson-${env.JOB_NAME}-${NODE_NAME}"
-        ])
+            checkout([
+                $class: 'TeamFoundationServerScm', 
+                projectPath: '$/UbigreenProject/UbigreenPerformance',
+                credentialsConfigurer: [$class: 'AutomaticCredentialsConfigurer'],
+                serverUrl: 'http://tls-ubi-tfs:8080/tfs/UbigreenCollection', 
+                useOverwrite: true,
+                workspaceName: "Hudson-${env.JOB_NAME}-${NODE_NAME}"
+            ])
+        }
     }
 }
