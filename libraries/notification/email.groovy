7f45b23537c7489e64e47c6131151556c16ae628
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-09-25 15:40:44 +0200

    Changed step

--- libraries/notification/email.groovy
+++ libraries/notification/email.groovy
@@ -1,4 +1,4 @@
-@Notify({context.step.equals(null) && context.status.equals("FAILURE")})
+@AfterStep({context.step.equals("build") && context.status.equals("FAILURE")})
 void call(context) {
     def emailSubject = "${env.JOB_NAME} - Build #${env.BUILD_NUMBER} - ${context.status}!"
     def emailBody = "Check console output at ${env.BUILD_URL} to view the results"
