54a91b9f8424a1b230b19e45b7a5710d981d326f
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-09-25 16:52:04 +0200

    remove node

--- libraries/resolve_dependencies/msbuild.groovy
+++ libraries/resolve_dependencies/msbuild.groovy
@@ -1,22 +1,11 @@
 @BeforeStep({ context.step.equals("build") })
 void call(context) {
-    println("test1")
-
-
     String sources = config.nugetSources.join(' -sources=')
-    println("test2")
-
-    String nugetPath = config.nugetPath
-    println("test3")
-    
+    String nugetPath = config.nugetPath    
     String projectPath = config.project
-    println("test4")
 
-    node {
-        stage('Resolve Dependencies: NuGet') {
-            println("test5")
-         
-            bat "\"${nugetPath}\" restore \"${projectPath}\" ${sources} -OutputDirectory ${env.WORKSPACE}\\packages"
-        }
+    stage('Resolve Dependencies: NuGet') {
+        println("test5")
+        bat "\"${nugetPath}\" restore \"${projectPath}\" ${sources} -OutputDirectory ${env.WORKSPACE}\\packages"
     }
 }
