6b46cb6a7d9bb7a84fde1c7c9da9238b7050d22d
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-07 17:51:29 +0200

    scm

--- libraries/tfs/checkout.groovy
+++ libraries/tfs/checkout.groovy
@@ -1,10 +1,3 @@
 void call() {
-    checkout([
-        $class: 'TeamFoundationServerScm', 
-        projectPath: '$/UbigreenProject/UbigreenPerformance',
-        credentialsConfigurer: [$class: 'AutomaticCredentialsConfigurer'],
-        serverUrl: 'http://tls-ubi-tfs:8080/tfs/UbigreenCollection', 
-        useOverwrite: true,
-        workspaceName: "Hudson-${env.JOB_NAME}-${NODE_NAME}"
-    ])
+    checkout scm
 }
