f39c53da1c220dc4af09d0cab491c146c3bc955a
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-07 15:28:52 +0200

    removed recipients

--- libraries/notification/email.groovy
+++ libraries/notification/email.groovy
@@ -2,5 +2,7 @@
 void call(context) {
     def emailSubject = "${env.JOB_NAME} - Build #${env.BUILD_NUMBER} - ${currentBuild.result.toString()}!"
     def emailBody = "Check console output at ${env.BUILD_URL} to view the results"
-    emailext body: emailBody, recipientProviders: [requestor(), buildUser(), developers(), recipients()], subject: emailSubject, from: "DevOps <devops@ubigreen.com>"
+    // emailext body: emailBody, recipientProviders: [requestor(), buildUser(), developers(), recipients()], subject: emailSubject, from: "DevOps <devops@ubigreen.com>"
+    emailext body: emailBody, recipientProviders: [recipients()], subject: emailSubject, from: "DevOps <devops@ubigreen.com>"
+
 }
