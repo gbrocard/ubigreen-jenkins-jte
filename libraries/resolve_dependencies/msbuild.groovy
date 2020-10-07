4109621cd7f324467145d4dfaf7c05404e53806f
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-07 10:52:47 +0200

    only workspace

--- libraries/resolve_dependencies/msbuild.groovy
+++ libraries/resolve_dependencies/msbuild.groovy
@@ -5,7 +5,7 @@ void call(context) {
     String projectPath = config.project
 
     stage('Resolve Dependencies: NuGet') {
-        print ("${context.WORKSPACE}")
-        bat "\"${nugetPath}\" restore \"${projectPath}\" ${sources} -OutputDirectory ${context.WORKSPACE}\\packages"
+        print ("${WORKSPACE}")
+        bat "\"${nugetPath}\" restore \"${projectPath}\" ${sources} -OutputDirectory ${WORKSPACE}\\packages"
     }
 }
