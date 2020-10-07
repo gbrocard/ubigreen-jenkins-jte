2538af00ae03f4f1afe2d96aeb673d92788fcd7f
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-07 15:23:03 +0200

    node

--- libraries/resolve_dependencies/node.groovy
+++ libraries/resolve_dependencies/node.groovy
@@ -1,17 +1,15 @@
 @BeforeStep({context.step.equals("build")})
 void call(context) {
-    node {
-        stage('Resolve dependencies: Node') {
-            dir("${env.WORKSPACE}/Ubigreen.PerformanceAnalyser.FullWeb") {
-                //remove readonly on package*.json
-                bat "attrib -r package.json && attrib -r package-lock.json && npm i"
-            }
-            post {
-                always {
-                    dir("${env.WORKSPACE}/Ubigreen.PerformanceAnalyser.FullWeb") {
-                        //add back readonly on package*.json
-                        bat "attrib +r package.json && attrib +r package-lock.json"
-                    }
+    stage('Resolve dependencies: Node') {
+        dir("${env.WORKSPACE}/Ubigreen.PerformanceAnalyser.FullWeb") {
+            //remove readonly on package*.json
+            bat "attrib -r package.json && attrib -r package-lock.json && npm i"
+        }
+        post {
+            always {
+                dir("${env.WORKSPACE}/Ubigreen.PerformanceAnalyser.FullWeb") {
+                    //add back readonly on package*.json
+                    bat "attrib +r package.json && attrib +r package-lock.json"
                 }
             }
         }
