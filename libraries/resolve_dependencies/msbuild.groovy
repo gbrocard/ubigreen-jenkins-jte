dc7ed03df0c43339e2a6792214755e879f86e814
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-07 15:20:56 +0200

    changes

--- libraries/resolve_dependencies/msbuild.groovy
+++ libraries/resolve_dependencies/msbuild.groovy
@@ -1,11 +1,15 @@
 @BeforeStep({ context.step.equals("build") })
 void call(context) {
+    print("test")
     String sources = config.nugetSources.join(' -sources=')
     String nugetPath = config.nugetPath    
     String projectPath = config.project
-
+    print("test2")
     stage('Resolve Dependencies: NuGet') {
-        def workspace = env.WORKSPACE
+        print("test3")
+        print("${env.WORKSPACE}")
+        String workspace = env.WORKSPACE
+        print("test4") 
         print ("WORKSPACE: ${workspace}")
         bat "\"${nugetPath}\" restore \"${projectPath}\" ${sources} -OutputDirectory ${workspace}\\packages"
     }
