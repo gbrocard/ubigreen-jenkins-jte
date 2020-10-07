0db1cd00b026d68006a38bb11e3cde0c34acb4af
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-07 17:30:54 +0200

    NODE IN CHECKOUT

--- libraries/tfs/checkout.groovy
+++ libraries/tfs/checkout.groovy
@@ -1,20 +1,20 @@
-void call() {
-    agent {label 'checkout'}
+void call(context) {
+    node {
+        stage('Checkout: TFS') {
+            options {
+                timeout(time: 15, unit: 'MINUTES')
+                retry(1)
+            }
 
-    stage('Checkout: TFS') {
-        options {
-            timeout(time: 15, unit: 'MINUTES')
-            retry(1)
+            checkout([
+                $class: 'TeamFoundationServerScm', 
+                projectPath: '$/UbigreenProject/UbigreenPerformance',
+                credentialsConfigurer: [$class: 'AutomaticCredentialsConfigurer'],
+                serverUrl: 'http://tls-ubi-tfs:8080/tfs/UbigreenCollection', 
+                useOverwrite: true,
+                workspaceName: "Hudson-test-test"
+                // workspaceName: "Hudson-${env.JOB_NAME}-${NODE_NAME}"
+            ])
         }
-
-        checkout([
-            $class: 'TeamFoundationServerScm', 
-            projectPath: '$/UbigreenProject/UbigreenPerformance',
-            credentialsConfigurer: [$class: 'AutomaticCredentialsConfigurer'],
-            serverUrl: 'http://tls-ubi-tfs:8080/tfs/UbigreenCollection', 
-            useOverwrite: true,
-            workspaceName: "Hudson-test-test"
-            // workspaceName: "Hudson-${env.JOB_NAME}-${NODE_NAME}"
-        ])
     }
 }
