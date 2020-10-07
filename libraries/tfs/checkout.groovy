622194d3976b3224fd98d34cbbe4a43e755006c0
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-07 17:55:34 +0200

    only checkout

--- libraries/tfs/checkout.groovy
+++ libraries/tfs/checkout.groovy
@@ -1,3 +1,12 @@
 void call() {
-    checkout scm
+    node {
+        checkout([
+            $class: 'TeamFoundationServerScm', 
+            projectPath: '$/UbigreenProject/UbigreenPerformance',
+            credentialsConfigurer: [$class: 'AutomaticCredentialsConfigurer'],
+            serverUrl: 'http://tls-ubi-tfs:8080/tfs/UbigreenCollection', 
+            useOverwrite: true,
+            workspaceName: "Hudson-test-test"
+        ])
+    }
 }
--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,18 +1,18 @@
-allow_scm_jenkinsfile = true 
+// allow_scm_jenkinsfile = true 
 
 libraries {
     merge = true
     tfs
-    resolve_dependencies {
-        nugetPath = 'C:\\Program Files (x86)\\NuGet\\nuget.exe'
-        nugetSources = ['https://api.nuget.org/v3/index.json', 'http://178.170.110.241:8080/nuget']
-        project = "${env.WORKSPACE}\\UbigreenPerformance.sln"
-    }
-    msbuild {
-        buildConfiguration = "Release"
-        testsPath = "${env.WORKSPACE}\\Tests"
-        testsResultPath = "${env.WORKSPACE}\\TestResults\\Jenkins\\${env.BUILD_TAG}"
-    }
-    // node
-    notification
+    // resolve_dependencies {
+    //     nugetPath = 'C:\\Program Files (x86)\\NuGet\\nuget.exe'
+    //     nugetSources = ['https://api.nuget.org/v3/index.json', 'http://178.170.110.241:8080/nuget']
+    //     project = "${env.WORKSPACE}\\UbigreenPerformance.sln"
+    // }
+    // msbuild {
+    //     buildConfiguration = "Release"
+    //     testsPath = "${env.WORKSPACE}\\Tests"
+    //     testsResultPath = "${env.WORKSPACE}\\TestResults\\Jenkins\\${env.BUILD_TAG}"
+    // }
+    // // node
+    // notification
 }
