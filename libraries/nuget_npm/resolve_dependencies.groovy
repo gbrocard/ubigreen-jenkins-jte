9438d62cdb5eb39a9dad3979d72e8e9c643f9f7e
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-09 15:52:53 +0200

    nuget npm

---
+++ libraries/nuget_npm/resolve_dependencies.groovy
@@ -0,0 +1,11 @@
+void call() {
+    stage('NuGet: Resolve dependencies') {
+        bat "\"${config.nuget_exe}\" restore \"%WORKSPACE%\\UbigreenPerformance.sln\" -source https://api.nuget.org/v3/index.json -source http://178.170.110.241:8080/nuget -OutputDirectory %WORKSPACE%\\packages"
+    }
+    stage('npm: Resolve dependencies') {
+        dir("%WORKSPACE%/Ubigreen.PerformanceAnalyser.FullWeb") {
+            //remove readonly on package*.json
+            bat "attrib -r package.json && attrib -r package-lock.json && npm i"
+        }
+    }
+}
--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,7 +1,9 @@
 libraries {
     tfs
-    nuget {
+    nuget_npm {
         nuget_exe = "C:\\Program Files (x86)\\NuGet\\nuget.exe"
+        NODE_PATH = "%WORKSPACE%\\Ubigreen.PerformanceAnalyser.FullWeb\\node_modules"
+        gulp = "%WORKSPACE%\\Ubigreen.PerformanceAnalyser.FullWeb\\node_modules\\gulp\\bin\\gulp.js"
     }
 }
 
