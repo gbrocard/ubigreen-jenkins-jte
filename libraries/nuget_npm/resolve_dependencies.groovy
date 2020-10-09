543e8ba90af188c8094c49ef41db82360d5d653e
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-09 16:07:25 +0200

    test

--- libraries/nuget_npm/resolve_dependencies.groovy
+++ libraries/nuget_npm/resolve_dependencies.groovy
@@ -3,7 +3,7 @@ void call() {
         bat "\"${config.nuget_exe}\" restore \"%WORKSPACE%\\UbigreenPerformance.sln\" -source https://api.nuget.org/v3/index.json -source http://178.170.110.241:8080/nuget -OutputDirectory %WORKSPACE%\\packages"
     }
     stage('npm: Resolve dependencies') {
-        dir("%WORKSPACE%/Ubigreen.PerformanceAnalyser.FullWeb") {
+        dir("Ubigreen.PerformanceAnalyser.FullWeb") {
             //remove readonly on package*.json
             bat (script: "npm i", returnStdout: true)
         }
