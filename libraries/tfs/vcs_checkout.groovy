c18f2092218091fc165c7478093ebbd857da36e3
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-09 15:23:16 +0200

    context + stage

--- libraries/tfs/vcs_checkout.groovy
+++ libraries/tfs/vcs_checkout.groovy
@@ -1,11 +1,13 @@
 @Init
-void call() {
-    checkout([
-        $class: 'TeamFoundationServerScm', 
-        projectPath: '$/UbigreenProject/UbigreenPerformance',
-        credentialsConfigurer: [$class: 'AutomaticCredentialsConfigurer'],
-        serverUrl: 'http://tls-ubi-tfs:8080/tfs/UbigreenCollection', 
-        useOverwrite: true,
-        workspaceName: "Hudson-${JOB_NAME}-${NODE_NAME}"
-    ])
+void call(context) {
+    stage("TFS: Checkout") {
+        checkout([
+            $class: 'TeamFoundationServerScm', 
+            projectPath: '$/UbigreenProject/UbigreenPerformance',
+            credentialsConfigurer: [$class: 'AutomaticCredentialsConfigurer'],
+            serverUrl: 'http://tls-ubi-tfs:8080/tfs/UbigreenCollection', 
+            useOverwrite: true,
+            workspaceName: "Hudson-${JOB_NAME}-${NODE_NAME}"
+        ])
+    }
 }
