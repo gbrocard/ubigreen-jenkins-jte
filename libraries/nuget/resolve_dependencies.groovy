56776136cf03518e85d9c3c6c6637a15d6bed8d8
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-09 15:48:52 +0200

    nuget exe

--- libraries/nuget/resolve_dependencies.groovy
+++ libraries/nuget/resolve_dependencies.groovy
@@ -1,5 +1,5 @@
 void call() {
     stage('NuGet: Resolve dependencies') {
-        bat "\"%NUGET%\" restore \"%WORKSPACE%\\UbigreenPerformance.sln\" -source https://api.nuget.org/v3/index.json -source http://178.170.110.241:8080/nuget -OutputDirectory %WORKSPACE%\\packages"
+        bat "\"${config.nuget_exe}\" restore \"%WORKSPACE%\\UbigreenPerformance.sln\" -source https://api.nuget.org/v3/index.json -source http://178.170.110.241:8080/nuget -OutputDirectory %WORKSPACE%\\packages"
     }
 }
--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,6 +1,8 @@
 libraries {
     tfs
-    nuget
+    nuget {
+        nuget_exe = "C:\\Program Files (x86)\\NuGet\\nuget.exe"
+    }
 }
 
 
