d9135e93e498f3c8c1472b9260088b44cbcc9de5
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-09 15:09:22 +0200

    test

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,18 +1,20 @@
-libraries {
-    merge = true
-    //tfs
-    // resolve_dependencies {
-    //     nugetPath = 'C:\\Program Files (x86)\\NuGet\\nuget.exe'
-    //     nugetSources = ['https://api.nuget.org/v3/index.json', 'http://178.170.110.241:8080/nuget']
-    //     project = "${env.WORKSPACE}\\UbigreenPerformance.sln"
-    // }
-    // msbuild {
-    //     buildConfiguration = "Release"
-    //     testsPath = "${env.WORKSPACE}\\Tests"
-    //     testsResultPath = "${env.WORKSPACE}\\TestResults\\Jenkins\\${env.BUILD_TAG}"
-    // }
-    // // node
-    // notification
-}
+allow_scm_jenkinsfile = true 
+
+// libraries {
+//     //merge = true
+//     //tfs
+//     // resolve_dependencies {
+//     //     nugetPath = 'C:\\Program Files (x86)\\NuGet\\nuget.exe'
+//     //     nugetSources = ['https://api.nuget.org/v3/index.json', 'http://178.170.110.241:8080/nuget']
+//     //     project = "${env.WORKSPACE}\\UbigreenPerformance.sln"
+//     // }
+//     // msbuild {
+//     //     buildConfiguration = "Release"
+//     //     testsPath = "${env.WORKSPACE}\\Tests"
+//     //     testsResultPath = "${env.WORKSPACE}\\TestResults\\Jenkins\\${env.BUILD_TAG}"
+//     // }
+//     // // node
+//     // notification
+// }
 
 
