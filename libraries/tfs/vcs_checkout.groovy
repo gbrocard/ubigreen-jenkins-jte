3252bdabbaef55f62bbad902a787a92c2b9a8891
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-09 15:21:57 +0200

    init +dependencies

--- libraries/tfs/vcs_checkout.groovy
+++ libraries/tfs/vcs_checkout.groovy
@@ -1,3 +1,4 @@
+@Init
 void call() {
     checkout([
         $class: 'TeamFoundationServerScm', 
--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,12 +1,12 @@
 libraries {
     tfs
-
+    resolve_dependencies {
+        nugetPath = 'C:\\Program Files (x86)\\NuGet\\nuget.exe'
+        nugetSources = ['https://api.nuget.org/v3/index.json', 'http://178.170.110.241:8080/nuget']
+        project = "${env.WORKSPACE}\\UbigreenPerformance.sln"
+    }
     //merge = true
-    // resolve_dependencies {
-    //     nugetPath = 'C:\\Program Files (x86)\\NuGet\\nuget.exe'
-    //     nugetSources = ['https://api.nuget.org/v3/index.json', 'http://178.170.110.241:8080/nuget']
-    //     project = "${env.WORKSPACE}\\UbigreenPerformance.sln"
-    // }
+
     // msbuild {
     //     buildConfiguration = "Release"
     //     testsPath = "${env.WORKSPACE}\\Tests"
