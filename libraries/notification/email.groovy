ca3895b2278d39cc2032b6fe5d6b713ddcc7b5f1
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-09 16:27:31 +0200

    test

--- libraries/notification/email.groovy
+++ libraries/notification/email.groovy
@@ -1,4 +1,8 @@
+//only executes at the end of the pipline and if in failure
 @Notify({ context.step == null && currentBuild.result == 'FAILURE' })
 void call(context) {
-    print("email")
+    def emailSubject = "${JOB_NAME} - Build #${BUILD_NUMBER} - ${currentBuild.currentResult}!"
+    def emailBody = "Check console output at ${BUILD_URL} to view the results"
+    // emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider'], [$class: 'DevelopersRecipientProvider']], to: '$DEFAULT_RECIPIENTS', subject: emailSubject, from: "DevOps <noreply@ubigreen.com>"
+    emailext attachLog: true, body: emailBody, to: "gbrocard@ubigreen.com", subject: emailSubject, from: "DevOps <noreply@ubigreen.com>"
 }
--- libraries/notification/slack.groovy
+++ libraries/notification/slack.groovy
@@ -1,3 +1,4 @@
+//only executes at the end of the pipline and if in failure
 @Notify({ context.step == null && currentBuild.result == 'FAILURE' })
 void call(context) {
     print("slack")
