adf5cc69eee9520a4533552b00dd6875e6986dbd
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-13 09:36:10 +0200

    test

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,9 +1,13 @@
 libraries {
     tfs
-    nuget_npm {
+    nuget {
         nuget_exe = "C:\\Program Files (x86)\\NuGet\\nuget.exe"
         NODE_PATH = "%WORKSPACE%\\Ubigreen.PerformanceAnalyser.FullWeb\\node_modules"
         gulp = "%WORKSPACE%\\Ubigreen.PerformanceAnalyser.FullWeb\\node_modules\\gulp\\bin\\gulp.js"
+        project = "UbigreenPerformance.sln"
+    }
+    npm {
+        projectPath = "Ubigreen.PerformanceAnalyser.FullWeb"
     }
     msbuild {
         MSBUILD_EXE = "C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
