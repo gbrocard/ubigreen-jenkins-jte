c7136ace6dcedc20270efa1f59e14fea96b79d9b
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-13 15:56:16 +0200

    Revert "test"
    This reverts commit adf5cc69eee9520a4533552b00dd6875e6986dbd.

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,13 +1,9 @@
 libraries {
     tfs
-    nuget {
+    nuget_npm {
         nuget_exe = "C:\\Program Files (x86)\\NuGet\\nuget.exe"
         NODE_PATH = "%WORKSPACE%\\Ubigreen.PerformanceAnalyser.FullWeb\\node_modules"
         gulp = "%WORKSPACE%\\Ubigreen.PerformanceAnalyser.FullWeb\\node_modules\\gulp\\bin\\gulp.js"
-        project = "UbigreenPerformance.sln"
-    }
-    npm {
-        projectPath = "Ubigreen.PerformanceAnalyser.FullWeb"
     }
     msbuild {
         MSBUILD_EXE = "C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
