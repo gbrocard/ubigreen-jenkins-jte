a8bc7a6e8eab2b4f197e6dc0f33d1639a4b48fbb
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-14 16:38:14 +0200

    recipients email

--- libraries/notification/email.groovy
+++ libraries/notification/email.groovy
@@ -3,6 +3,5 @@
 void call(context) {
     def emailSubject = "${JOB_NAME} - Build #${BUILD_NUMBER} - ${currentBuild.currentResult}!"
     def emailBody = "Check console output at ${BUILD_URL} to view the results"
-    // emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider'], [$class: 'DevelopersRecipientProvider']], to: '$DEFAULT_RECIPIENTS', subject: emailSubject, from: "DevOps <noreply@ubigreen.com>"
-    emailext attachLog: true, body: emailBody, to: "gbrocard@ubigreen.com", subject: emailSubject, from: "DevOps <noreply@ubigreen.com>"
+    emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider'], [$class: 'DevelopersRecipientProvider']], to: '$DEFAULT_RECIPIENTS', subject: emailSubject, from: "DevOps <noreply@ubigreen.com>"
 }
