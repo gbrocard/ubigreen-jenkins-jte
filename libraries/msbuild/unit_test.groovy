d4994f79715df5002f16922225d506b991d0d8f4
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-14 14:41:57 +0200

    print

--- libraries/msbuild/unit_test.groovy
+++ libraries/msbuild/unit_test.groovy
@@ -2,6 +2,7 @@ void call() {
     stage("MSBuild: Unit tests") {
         def allTestsNames = bat(script: "dir /B \"%WORKSPACE%\\${config.TESTS_PATH}\"", returnStdout: true).split("\n")
         def workspace = pwd()
+        print(workspace)
         //map the projects name with their corresponding DLL
         def testDllMap = [:]
         for(test in allTestsNames) {
