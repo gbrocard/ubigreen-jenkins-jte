53c2ec5013b1b9ecf8a62ab4526d2af0ce001bf7
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-15 16:52:48 +0200

    test

--- libraries/jenkins_jobs/chain_job.groovy
+++ libraries/jenkins_jobs/chain_job.groovy
@@ -1,8 +1,6 @@
 @Notify({ context.step == null && currentBuild.currentResult == 'SUCCESS' })
 void call(context) {
-    stage("Job: Launching " + config.jobName) {
-        print("TEST WORKSPACE ${WORKSPACE}")
+    print("TEST WORKSPACE $WORKSPACE")
 
-        build wait: false, job: config.jobName, parameters: [string(name: 'upstreamWorkspace', value: $WORKSPACE)]
-    }
+    build wait: false, job: config.jobName, parameters: [string(name: 'upstreamWorkspace', value: $WORKSPACE)]
 }
