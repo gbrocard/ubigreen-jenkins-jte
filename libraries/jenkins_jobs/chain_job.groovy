9f088fc91589923513664a1369a9b8fa019f59a0
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-15 16:03:30 +0200

    notify

--- libraries/jenkins_jobs/chain_job.groovy
+++ libraries/jenkins_jobs/chain_job.groovy
@@ -1,4 +1,4 @@
-@CleanUp({ currentBuild.currentResult == 'SUCCESS' })
+@Notify({ context.step == null && currentBuild.currentResult == 'SUCCESS' })
 void call(context) {
     def workspace = pwd()
     print("TEST WORKSPACE ${workspace}")
