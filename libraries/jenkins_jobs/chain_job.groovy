7d8ab04ab9ade7627c75dffdaa172fee126820d0
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-15 15:09:09 +0200

    pwd

--- libraries/jenkins_jobs/chain_job.groovy
+++ libraries/jenkins_jobs/chain_job.groovy
@@ -1,6 +1,4 @@
 @CleanUp({ currentBuild.result == 'SUCCESS' })
 void call(context) {
-    def workspace = pwd()
-    print("WORKSPACE PARAM : ${workspace}")
-    build wait: false, job: config.jobName, parameters: [[$class: 'StringParameterValue', name: 'upstreamWorkspace', value: workspace]]
+    build wait: false, job: config.jobName, parameters: [string(name: 'upstreamWorkspace', value: pwd())]
 }
