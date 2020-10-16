f0429f969a76637ddd47c92930b8a16638eea759
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-16 16:36:52 +0200

    fix typo

--- libraries/cypress/integration_tests.groovy
+++ libraries/cypress/integration_tests.groovy
@@ -2,7 +2,7 @@ void call() {
     stage("Cypress: Integration tests") {
         dir (config.projectPath) {
             try {
-                bat "npx cypress run --confing video=false --reporter junit --reporter-options \"mochaFile=cypress/results/Ubigreen.PerformanceAnalyser.FullWeb.xml\""
+                bat "npx cypress run --config video=false --reporter junit --reporter-options \"mochaFile=cypress/results/Ubigreen.PerformanceAnalyser.FullWeb.xml\""
             } catch (Exception e) {
                 print(e.getMessage())
             }
