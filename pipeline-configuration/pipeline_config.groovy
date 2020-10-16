4e78cd9feda5e300d8b2bf308b1350ce9344505f
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-16 14:33:29 +0200

    remove params

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -8,8 +8,6 @@ libraries {
     tfs
     nuget_npm {
         nuget_exe = "C:\\Program Files (x86)\\NuGet\\nuget.exe"
-        NODE_PATH = "%WORKSPACE%\\Ubigreen.PerformanceAnalyser.FullWeb\\node_modules"
-        gulp = "%WORKSPACE%\\Ubigreen.PerformanceAnalyser.FullWeb\\node_modules\\gulp\\bin\\gulp.js"
     }
     msbuild {
         MSBUILD_EXE = "C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
