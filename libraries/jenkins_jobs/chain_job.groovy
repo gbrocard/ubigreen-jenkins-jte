43a102f6583c953e449f9b35d861954b92ba7230
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-16 11:00:18 +0200

    job

--- libraries/jenkins_jobs/chain_job.groovy
+++ libraries/jenkins_jobs/chain_job.groovy
@@ -1,6 +1,3 @@
 void call(applicationEnv) {
-    def workspace = pwd()
-    print(workspace)
-    print(applicationEnv.jobName)
-    build wait: true, job: applicationEnv.jobName, parameters: [string(name: 'upstreamWorkspace', value: workspace)]
+    build job: applicationEnv.jobName, parameters: [string(name: 'upstreamWorkspace', value: workspace)]
 }
