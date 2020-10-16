3116805fe30950663c3b58f9ec1f0b22359cc21a
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-16 11:29:23 +0200

    fix workspace

--- libraries/jenkins_jobs/chain_job.groovy
+++ libraries/jenkins_jobs/chain_job.groovy
@@ -1,5 +1,3 @@
 void call(applicationEnv) {
-    step("Job: " + applicationEnv.jobName) {
-        build job: applicationEnv.jobName, parameters: [string(name: 'upstreamWorkspace', value: workspace)]
-    }
+    build job: applicationEnv.jobName, parameters: [string(name: 'upstreamWorkspace', value: "${pwd()}")]
 }
