ccef0438382073fd569e34ec35749dbc2fa4a986
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-20 17:03:42 +0200

    default list

--- libraries/notification/email.groovy
+++ libraries/notification/email.groovy
@@ -1,8 +1,8 @@
 //only executes at the end of the pipline and if in failure && currentBuild.result == 'FAILURE' 
-@Notify({ context.step == null })
+@Notify({ context.step == null && currentBuild.result == 'FAILURE' })
 void call(context) {
     def emailSubject = "${JOB_NAME} - Build #${BUILD_NUMBER} - ${currentBuild.currentResult}!"
     def emailBody = "Check console output at ${BUILD_URL} to view the results"
-    emailext attachLog: true, body: emailBody, to: 'thuguet@ubigreen.com,gbrocard@ubigreen.com,lmarfaing@ubigreen.com,wcourtade@ubigreen.com,npunti@ubigreen.com,eelaoud@ubigreen.com', subject: emailSubject, from: "DevOps <noreply@ubigreen.com>"
-    // emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider'], [$class: 'DevelopersRecipientProvider']], to: '$DEFAULT_RECIPIENTS', subject: emailSubject, from: "DevOps <noreply@ubigreen.com>"
+    // emailext attachLog: true, body: emailBody, to: 'gbrocard@ubigreen.com', subject: emailSubject, from: "DevOps <noreply@ubigreen.com>"
+    emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider'], [$class: 'DevelopersRecipientProvider']], to: '$DEFAULT_RECIPIENTS', subject: emailSubject, from: "DevOps <noreply@ubigreen.com>"
 }
