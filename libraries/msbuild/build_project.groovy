8e06f4b3f839e81cc85742bab0532e24cf868952
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-20 16:23:12 +0200

    test email

--- libraries/msbuild/build_project.groovy
+++ libraries/msbuild/build_project.groovy
@@ -1,5 +1,9 @@
 void call() {
     stage("MSBuild: Build") {
+        def emailSubject = "${JOB_NAME} - Build #${BUILD_NUMBER} - ${currentBuild.currentResult}!"
+        def emailBody = "Check console output at ${BUILD_URL} to view the results"
+        emailext attachLog: true, body: emailBody, to: 'gbrocard@ubigreen.com', subject: emailSubject, from: "DevOps <noreply@ubigreen.com>"
+        
         bat "\"${config.MSBUILD_EXE}\" %WORKSPACE%\\UbigreenPerformance.sln /p:Configuration=Release /p:WarningLevel=0 /m"
     }
 }
