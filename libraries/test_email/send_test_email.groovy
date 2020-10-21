b528b2a8782da747654ae93d9f14dea4ec1b98ad
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-21 11:30:32 +0200

    fix tilbauld

--- libraries/test_email/send_test_email.groovy
+++ libraries/test_email/send_test_email.groovy
@@ -1,5 +1,5 @@
 void call() {
     def emailSubject = "${JOB_NAME} - Build #${BUILD_NUMBER} - ${currentBuild.currentResult}!"
     def emailBody = "Check console output at ${BUILD_URL} to view the results"
-    emailext attachLog: true, body: emailBody, to: 'gbrocard@ubigreen.com,wcourtade@ubigreen.com,eelaoud@ubigreen.com,npunti@ubigreen.com,thughuet@ubigreen.com,lmarfaing@ubigreen.com,mleitao@ubigreen.com', subject: emailSubject, from: "DevOps <team-solution@ubigreen.com>"
+    emailext attachLog: true, body: emailBody, to: 'gbrocard@ubigreen.com,wcourtade@ubigreen.com,eelaoud@ubigreen.com,npunti@ubigreen.com,thuguet@ubigreen.com,lmarfaing@ubigreen.com,mleitao@ubigreen.com', subject: emailSubject, from: "DevOps <team-solution@ubigreen.com>"
 }
