af9b00c8a5b1fc4028a09cad7fa898702c4a813b
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-21 18:03:28 +0200

    added test regression email

--- libraries/notification/email.groovy
+++ libraries/notification/email.groovy
@@ -1,8 +1,18 @@
 //only executes at the end of the pipline and if in failure
-@Notify({ context.step == null && currentBuild.result == 'FAILURE' })
+@Notify({ context.step == null })
 void call(context) {
-    def emailSubject = "${JOB_NAME} - Build #${BUILD_NUMBER} - ${currentBuild.currentResult}!"
-    def emailBody = "Check console output at ${BUILD_URL} to view the results"
-    // emailext attachLog: true, body: emailBody, to: 'gbrocard@ubigreen.com', subject: emailSubject, from: "DevOps <noreply@ubigreen.com>"
-    emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider'], [$class: 'DevelopersRecipientProvider']], to: '$DEFAULT_RECIPIENTS', subject: emailSubject, from: "DevOps <team-solution@ubigreen.com>"
+    def jobName = "${JOB_NAME}"
+    def isTestJob = jobName.toLowerCase().contains("test")
+
+    if (currentBuild.result == 'FAILURE') {
+        def emailSubject = "${JOB_NAME} - Build #${BUILD_NUMBER} - ${currentBuild.currentResult}!"
+        def emailBody = "Check console output at ${BUILD_URL} to view the results"
+        emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider'], [$class: 'DevelopersRecipientProvider']], to: '$DEFAULT_RECIPIENTS', subject: emailSubject, from: "DevOps <team-solution@ubigreen.com>"
+    } else if (isTestJob) {
+        def emailSubject = "Tests regression on ${JOB_NAME} - Build #${BUILD_NUMBER}!"
+        def emailBody = "Check console output at ${BUILD_URL} to view the results"
+        emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'FailingTestSuspectsRecipientProvider']], to: '$DEFAULT_RECIPIENTS', subject: emailSubject, from: "DevOps <team-solution@ubigreen.com>"
+    } else {
+        print ("No email to send")
+    }
 }
