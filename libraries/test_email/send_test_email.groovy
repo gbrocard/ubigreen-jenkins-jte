221f8b63cacff2e61789d9e125b98aa6490f338c
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-22 14:56:25 +0200

    Revert "email regression"
    This reverts commit 45c4b289bf7cbe5a44754644c210065ab9c3d853.

--- libraries/test_email/send_test_email.groovy
+++ libraries/test_email/send_test_email.groovy
@@ -5,16 +5,18 @@ void call() {
     if (currentBuild.result == 'FAILURE') {
         def emailSubject = "${JOB_NAME} - Build #${BUILD_NUMBER} - ${currentBuild.currentResult}!"
         def emailBody = "Check console output at ${BUILD_URL} to view the results"
-        emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider'], [$class: 'DevelopersRecipientProvider']], to: '$DEFAULT_RECIPIENTS', subject: emailSubject, from: "DevOps <team-solution@ubigreen.com>"
+        print("FAILURE")
+        // emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider'], [$class: 'DevelopersRecipientProvider']], to: '$DEFAULT_RECIPIENTS', subject: emailSubject, from: "DevOps <team-solution@ubigreen.com>"
     } else if (isTestJob) {
         def emailSubject = "Tests regression on ${JOB_NAME} - Build #${BUILD_NUMBER}!"
         def emailBody = "Check console output at ${BUILD_URL} to view the results"
 
         if (isRegression()) {
-            emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'FailingTestSuspectsRecipientProvider']], to: '$DEFAULT_RECIPIENTS' subject: emailSubject, from: "DevOps <team-solution@ubigreen.com>"
+            print("REGRESSION !")
         } else {
-            print("No regression found")
+            print("no regression")
         }
+        // emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'FailingTestSuspectsRecipientProvider']], subject: emailSubject, from: "DevOps <team-solution@ubigreen.com>"
     } else {
         print ("No email to send")
     }
@@ -26,7 +28,7 @@ def isRegression() {
 
     def previousBuild = currentBuild.rawBuild.getPreviousNotFailedBuild()
     if (previousBuild == null) {
-        print("No previous successfull or unstable build was found")
+        print("No previous successfull or unstable build")
         return false;
     } 
 
@@ -36,7 +38,8 @@ def isRegression() {
     def currentBuildFailedTestNumber = currentBuild.rawBuild.getAction(hudson.tasks.junit.TestResultAction.class)?.getFailCount()
     def currentBuildFailedTests =  currentBuild.rawBuild.getAction(hudson.tasks.junit.TestResultAction.class)?.getFailedTests()
 
-    print("Comparing tests to build nb ${previousBuild.number}. Previous failed tests : ${previousBuildFailedTestNumber}, current failed tests : ${currentBuildFailedTestNumber}")
+    print("Previous nb : ${previousBuildFailedTestNumber} build: ${previousBuild.number}")
+    print("Current nb : ${currentBuildFailedTestNumber}")
 
     //si on a + de tests en failure ou si les tests en failure ont changés
     return (currentBuildFailedTestNumber > previousBuildFailedTestNumber || !testsAreEqual(currentBuildFailedTests, previousBuildFailedTests))
@@ -55,17 +58,16 @@ def testsAreEqual(currentTestList, previousTestList) {
         def currentTest = currentTestList[i];
         def previousTest = previousTestList.find { it.getFullName() == currentTest.getFullName() };
 
-        // new test in failure
+        // test is not found
         if (previousTest == null) {
-            print("ERROR : ${currentTest.getFullName()} NOT FOUND");
+            print("ERROR : ${currentBuild.getFullName()} NOT FOUND");
             return false;
         }
-
+        print(currentTest.getErrorDetails())
         // compare error messages
         if (currentTest.getErrorDetails() != previousTest.getErrorDetails()) {
-            print("----------------------------------------- REGRESSION FOUND -----------------------------------------")
-            print("Previous error message : ${previousTest.getErrorDetails()}")
             print("Current error message : ${currentTest.getErrorDetails()}")
+            print("Previous error message : ${previousTest.getErrorDetails()}")
 
             return false;
         }
