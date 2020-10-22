9845bcad63c00b782530cbc03a2c1f3a01802914
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-22 10:32:58 +0200

    regression

--- libraries/test_email/send_test_email.groovy
+++ libraries/test_email/send_test_email.groovy
@@ -1,17 +1,26 @@
 void call() {
     def jobName = "${JOB_NAME}"
-    print(jobName)
     def isTestJob = jobName.toLowerCase().contains("test")
 
-    if (isTestJob) {
-        def emailSubject = "Tests regression on ${JOB_NAME} - Build #${BUILD_NUMBER}!"
-        def emailBody = "Check console output at ${BUILD_URL} to view the results"
-        // emailext attachLog: true, body: emailBody, to: 'gbrocard@ubigreen.com', subject: emailSubject, from: "DevOps <noreply@ubigreen.com>"
-        emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'FailingTestSuspectsRecipientProvider']], to: 'gbrocard@ubigreen.com', subject: emailSubject, from: "DevOps <team-solution@ubigreen.com>"
-    } else {
+    if (currentBuild.result == 'FAILURE') {
         def emailSubject = "${JOB_NAME} - Build #${BUILD_NUMBER} - ${currentBuild.currentResult}!"
         def emailBody = "Check console output at ${BUILD_URL} to view the results"
-        // emailext attachLog: true, body: emailBody, to: 'gbrocard@ubigreen.com', subject: emailSubject, from: "DevOps <noreply@ubigreen.com>"
-        emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider'], [$class: 'DevelopersRecipientProvider'], [$class: 'FailingTestSuspectsRecipientProvider']], to: 'gbrocard@ubigreen.com', subject: emailSubject, from: "DevOps <team-solution@ubigreen.com>"
+        emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider'], [$class: 'DevelopersRecipientProvider']], to: '$DEFAULT_RECIPIENTS', subject: emailSubject, from: "DevOps <team-solution@ubigreen.com>"
+    } else if (isTestJob) {
+        def emailSubject = "Tests regression on ${JOB_NAME} - Build #${BUILD_NUMBER}!"
+        def emailBody = "Check console output at ${BUILD_URL} to view the results"
+
+        def previousBuildFailedTestNumber = currentBuild.getPreviousSuccessfulBuild()?.getAction(hudson.tasks.junit.TestResultAction.class).getFailCount()
+        def previousBuildFailedTests = currentBuild.getPreviousSuccessfulBuild()?.getAction(hudson.tasks.junit.TestResultAction.class).getFailedTests()
+        def currentBuildFailedTestNumber = currentBuild.getAction(hudson.tasks.junit.TestResultAction.class).getFailCount()
+        def currentBuildFailedTests =  currentBuild.getAction(hudson.tasks.junit.TestResultAction.class).getFailedTests()
+        
+        //si on a + de tests en failure ou si les tests en failure ont changés
+        if (currentBuildFailedTestNumber > previousBuildFailedTestNumber || !(previousBuildFailedTests.equals(currentBuildFailedTests))) {
+            print("REGRESSION !")
+        }
+        // emailext attachLog: true, body: emailBody, recipientProviders: [[$class: 'FailingTestSuspectsRecipientProvider']], subject: emailSubject, from: "DevOps <team-solution@ubigreen.com>"
+    } else {
+        print ("No email to send")
     }
 }
