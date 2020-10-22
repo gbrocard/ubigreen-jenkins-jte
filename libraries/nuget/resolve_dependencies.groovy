68aac08fa3d6cbe0354ead2d893d495645467651
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-22 16:27:38 +0200

    fix path

--- libraries/nuget/resolve_dependencies.groovy
+++ libraries/nuget/resolve_dependencies.groovy
@@ -1,5 +1,5 @@
 void call() {
     stage('NuGet: Resolve dependencies') {
-        bat "\"${config.nuget_exe}\" restore \"%WORKSPACE%\\${config.project}\" -source https://api.nuget.org/v3/index.json -source http://178.170.110.241:8080/nuget -OutputDirectory %WORKSPACE%\\packages"
+        bat "\"${config.nuget_exe}\" restore \"%WORKSPACE%\\${config.projectPath}\" -source https://api.nuget.org/v3/index.json -source http://178.170.110.241:8080/nuget -OutputDirectory %WORKSPACE%\\packages"
     }
 }
