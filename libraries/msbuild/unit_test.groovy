9c6a2b673c7fa766e7760c5b5f29e22368098612
Guillaume Brocard <gbrocard@UBIGREEN-FR.COM>
2020-10-22 10:53:44 +0200

    rm print

--- libraries/msbuild/unit_test.groovy
+++ libraries/msbuild/unit_test.groovy
@@ -1,7 +1,5 @@
 void call() {
     stage("MSBuild: Unit tests") {
-        print("WOKSPACE PARAM : ${params.upstreamWorkspace}")
-
         ws(params.upstreamWorkspace) {
             def allTestsNames = bat(script: "dir /B \"%WORKSPACE%\\${config.TESTS_PATH}\"", returnStdout: true).split("\n")
             def workspace = pwd()
